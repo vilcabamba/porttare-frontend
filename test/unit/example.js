ec4c22b3990c79db70a3d12441307906ecccadc1
Fabricio Flores <fabrifloresg@gmail.com>
2016-05-30 18:55:42 -0500

    Add circle.yml and get rid of protractor

---
+++ test/unit/example.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  describe('foo controller', function () {
+    it('boo function', function(){
+      console.log(chai.expect);
+      chai.expect(true).to.be.equal(true);
+    });
+  });
+})();
