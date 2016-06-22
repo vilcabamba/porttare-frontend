981fc419fc35c9c01bdbc46e04951ab4aa6f8b6f
Jhymer Martínez <jmartinez@ioet.com>
2016-06-22 01:29:21 -0500

    Add places search box

--- app/scripts/controllers/map-controller.js
+++ app/scripts/controllers/map-controller.js
@@ -34,6 +34,8 @@
           map: map
         });
 
+        loadPlacesSearchBox(map);
+
         $ionicLoading.hide();
 
       }, function error() {
@@ -43,6 +45,55 @@
           template: 'Hubo un error al cargar el mapa.'
         });
       });
+
+    function loadPlacesSearchBox(map) {
+      var input = document.getElementById('input-places');
+      var placesSearchBox = new google.maps.places.SearchBox(input);
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+      var markers = [];
+
+      placesSearchBox.addListener('places_changed', function () {
+        var places = placesSearchBox.getPlaces();
+
+        if (places.length === 0) {
+          return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+          marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+          var icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+          // Create a marker for each place.
+          markers.push(new google.maps.Marker({
+            map: map,
+            icon: icon,
+            title: place.name,
+            position: place.geometry.location
+          }));
+
+          if (place.geometry.viewport) {
+            bounds.union(place.geometry.viewport);
+          } else {
+            bounds.extend(place.geometry.location);
+          }
+        });
+        map.fitBounds(bounds);
+      });
+    }
 
   }
 })();
