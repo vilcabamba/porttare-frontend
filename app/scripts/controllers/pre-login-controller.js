934db20b3e7e523df800ddd9b5e58d61b3310108
Fabricio Flores <fabrifloresg@gmail.com>
2016-06-28 21:48:18 -0500

    Add facebook login in prelogin

--- app/scripts/controllers/pre-login-controller.js
+++ app/scripts/controllers/pre-login-controller.js
@@ -4,7 +4,22 @@ angular
   .module('porttare.controllers')
   .controller('PreController', PreController);
 
-function PreController() {
+function PreController($auth, $state, $ionicPopup) {
   var preVm = this;
+  preVm.loginWithFB = loginWithFB;
+  var successState = 'app.playlists';
+
+  function loginWithFB() {
+    $auth.authenticate('facebook')
+      .then(function () {
+        $state.go(successState);
+      })
+      .catch(function () {
+        $ionicPopup.alert({
+          title: 'Error',
+          template: 'Hubo un error, intentalo nuevamente.'
+        });
+      });
+  }
 
 }
