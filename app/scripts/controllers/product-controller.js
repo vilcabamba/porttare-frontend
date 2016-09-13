c2c732a9b3fd1b45e95ce2a93fdbfcbf2c029940
Darwin Gualotuña <daosgc@gmail.com>
2016-09-13 10:53:06 -0500

    Add style in product page

--- app/scripts/controllers/product-controller.js
+++ app/scripts/controllers/product-controller.js
@@ -7,13 +7,34 @@
 
   function ProductController() {
     var productVm = this;
+    productVm.more = false;
 
     productVm.product = {
       id: 1,
       name: 'provider1',
       description: 'restaurant',
       image: '../images/bg.png',
-      more: 'more information'
+      more: 'more information',
+      amount: 0,
+      unit_price: 2.5,
+      total_price: 0
+    };
+
+    productVm.show_more = function () {
+      if(productVm.more){
+        productVm.more = false;
+      }else{
+        productVm.more = true;
+      }
+    };
+
+    productVm.updatePrice = function (opt) {
+      if(opt === 'add'){
+        productVm.product.amount ++;
+      }else if(opt === 'remove'){
+        productVm.product.amount --;
+      }
+      productVm.product.total_price = (productVm.product.amount * productVm.product.unit_price);
     };
   }
 })();
