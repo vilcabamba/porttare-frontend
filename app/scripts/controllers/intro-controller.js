6a69e9ac1e287579ea1bf46d79443e50e488d383
Mario Andrés Correa <a@macool.me>
2016-11-23 16:38:25 -0500

    mostrar StatusBar en pantalla de intro
    y deshacerse del viejo plugin de córdova com.ionic.keyboard

--- app/scripts/controllers/intro-controller.js
+++ app/scripts/controllers/intro-controller.js
@@ -5,14 +5,30 @@
     .module('porttare.controllers')
     .controller('IntroController', IntroController);
 
-  function IntroController($state, $ionicSlideBoxDelegate, $localStorage) {
+  function IntroController($state,
+                           $localStorage,
+                           $ionicPlatform,
+                           $ionicSlideBoxDelegate) {
     var introVm = this;
     introVm.startApp = startApp;
     introVm.next = next;
     introVm.previous = previous;
     introVm.slideChanged = slideChanged;
 
+    $ionicPlatform.ready(traslucentStatusBar);
+
+    function traslucentStatusBar() {
+      // this view is dark. make status bar visible
+      window.StatusBar && window.StatusBar.styleBlackTranslucent();
+    }
+
+    function normalStatusBar() {
+      // return to default status bar style for the rest of the app
+      window.StatusBar && window.StatusBar.styleDefault();
+    }
+
     function startApp() {
+      normalStatusBar();
       $localStorage.set('hasViewedTutorial','true');
       $state.go('prelogin');
     }
