bb80692540bddf122b3b7cdd752e4727734e6321
Mario Andrés <a@macool.me>
2016-11-27 07:03:33 -0500

    UI enhances (#95)
    * revise styles for provider form
    
    * enhance category view for only one provider
    
    * upgrade ionic & force JS scrolling over native scrolling
    
    * enhance UI

--- app/scripts/controllers/category-controller.js
+++ app/scripts/controllers/category-controller.js
@@ -10,10 +10,16 @@
 
     categoryVm.category = data.provider_category;//jshint ignore:line
     categoryVm.providers = data.provider_category.provider_profiles;//jshint ignore:line
+
+    // use centerMode only if there's more than
+    // one provider. Otherwise unique provider
+    // is moved to the left
+    var centerMode = categoryVm.providers.length > 1;
+
     categoryVm.slickConfig = {
       slidesToShow: 1,
       arrows: false,
-      centerMode: true,
+      centerMode: centerMode,
       mobileFirst: true,
       swipeToSlide: true,
       responsive: [
@@ -26,13 +32,15 @@
         {
           breakpoint: 700,
           settings: {
-            slidesToShow: 3
+            slidesToShow: 3,
+            centerMode: false
           }
         },
         {
           breakpoint: 1000,
           settings: {
-            slidesToShow: 4
+            slidesToShow: 4,
+            centerMode: false
           }
         }
       ]
