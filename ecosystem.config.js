3392a6de71c3566129cb1cd881c2d0d6ef582ca4
Mario Andrés Correa <a@macool.me>
2016-11-27 22:34:08 -0500

    testing deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       ref  : "origin/production",
       repo : "git@github.com:noggalito/porttare-frontend.git",
       path : "~/demo-porttare-frontend",
-      "post-deploy" : "export PATH=~/.nvm/versions/v0.12.14/bin:~/.rbenv/shims:$PATH && npm install && NODE_ENV=demo ./node_modules/grunt-cli/bin/grunt compress && pm2 startOrRestart ~/demo-porttare-frontend/shared/ecosystem.json"
+      "post-deploy" : "export PATH=~/.nvm/versions/v0.12.14/bin:~/.rbenv/shims:$PATH && npm install && pm2 startOrRestart ~/demo-porttare-frontend/shared/ecosystem.json"
     }
   }
 }
