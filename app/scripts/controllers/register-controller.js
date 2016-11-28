6ed1bdc79c655cd93e97bd2e3421d97bf31be5ce
Fabricio Flores <fabrifloresg@gmail.com>
2016-11-28 12:23:52 +0100

    Fix register form. Remove double popup from 401

--- app/scripts/controllers/register-controller.js
+++ app/scripts/controllers/register-controller.js
@@ -13,38 +13,35 @@
                               LoginService) {
     var registerVm = this;
     registerVm.register = register;
-    registerVm.registerForm = {};
     registerVm.loginWithFB = LoginService.loginWithFB;
 
     function register() {
-      $ionicLoading.show({
-        template: 'cargando...'
-      });
-      $auth.submitRegistration(registerVm.registerForm)
-        .then(function() {
-          $state.go('login').then(function(){
-            $ionicPopup.alert({
-              title: 'Alerta',
-              template: 'Usuario creado satisfactoriamente'
-            });
-          });
-        })
-        .finally(function(){
-          $ionicLoading.hide();
+      if (!registerVm.registerForm.$invalid) {
+        $ionicLoading.show({
+          template: 'cargando...'
         });
+        $auth.submitRegistration(registerVm.user)
+          .then(function() {
+            $state.go('login').then(function(){
+              $ionicPopup.alert({
+                title: 'Alerta',
+                template: 'Usuario creado satisfactoriamente'
+              });
+            });
+          })
+          .finally(function(){
+            $ionicLoading.hide();
+          });
+      }else{
+        $ionicPopup.alert({
+          title: 'Alerta',
+          template: 'Error en el formulario'
+        });
+      }
     }
 
     $scope.$on('auth:registration-email-error', function(event, response){
-      var errors;
-      if (response.errors.full_messages) { // jshint ignore:line
-        errors = response.errors.full_messages.join(', '); // jshint ignore:line
-      }else{
-        errors = response.errors.join(', ');
-      }
-      $ionicPopup.alert({
-        title: 'Error',
-        template: errors
-      });
+      registerVm.errors = response.errors;
     });
 
   }
