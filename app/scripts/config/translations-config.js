ecb46c2ef8d9b215098f618d95cefe7079117042
Fabricio Flores <fabrifloresg@gmail.com>
2016-11-29 18:06:35 +0100

    Revert "Feature #798"
    This reverts commit cd6e3c516ae35b146a91e5f769e43de8d8de2c11.

--- app/scripts/config/translations-config.js
+++ app/scripts/config/translations-config.js
@@ -159,8 +159,6 @@
         subtitleWithoutItems: 'Da click en Nuevo Producto para empezar a vender.',
         titulo: 'Nombre del producto',
         descripcion: 'Descripción corta',
-        cantidad: 'Cantidad en stock',
-        stock:'Producto en Stock',
         precio: 'Precio',
         volumen: 'Volumen',
         peso: 'Peso',
