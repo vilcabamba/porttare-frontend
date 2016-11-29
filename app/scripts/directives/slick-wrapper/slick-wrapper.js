959ee18d34c57a5ea38e11b7154873959d932826
Jhymer Martínez <jmartinez@ioet.com>
2016-11-29 03:13:42 -0500

    Add slick-provider directive (#117)

---
+++ app/scripts/directives/slick-wrapper/slick-wrapper.js
@@ -0,0 +1,36 @@
+(function () {
+  'use strict';
+  /* jshint validthis:true */
+  angular
+    .module('porttare.directives')
+    .directive('slickWrapper', slickWrapper);
+
+  function slickWrapper() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'templates/directives/slick-wrapper/slick-wrapper.html',
+      scope: {
+        settings: '='
+      },
+      controller: slickWrapperController,
+      controllerAs: 'swcVm',
+      bindToController: true,
+      transclude: true
+    };
+
+    return directive;
+  }
+
+  function slickWrapperController() {
+    var swcVm = this;
+    var defaultSlickConfig = {
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      swipeToSlide: true
+    };
+    swcVm.customSettings = angular.merge({}, defaultSlickConfig, swcVm.settings);
+
+  }
+})();
