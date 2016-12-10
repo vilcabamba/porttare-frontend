ead3720b7fac2ce46d1c81bf6f074de0d3601193
carito22 <carito22@users.noreply.github.com>
2016-12-10 17:40:47 -0500

    Bug#826 CONDICION CUANDO FECHA DE NACIMIENTO DE COMPRADOR Y WISHLIST ES NULL (#152)
    * comprador-editar perfil- fecha nacimiento
    
    * remplazado por toDate()
    
    * condicion para null
    
    * jshint corregido
    
    * corregido cuando no hay fecha

--- app/scripts/controllers/profile/profile-info-controller.js
+++ app/scripts/controllers/profile/profile-info-controller.js
@@ -26,8 +26,10 @@
 
     function showNewModal() {
       piVm.userEdit = angular.copy(piVm.user);
-      var fecha_nac = moment(piVm.user.fecha_nacimiento, 'YYYY/MM/DD HH:mm Z');//jshint ignore: line
-      piVm.userEdit.fecha_nacimiento = fecha_nac.toDate(); //jshint ignore: line
+      if(piVm.user.fecha_nacimiento){//jshint ignore: line
+        var fecha_nac = moment(piVm.user.fecha_nacimiento, 'YYYY/MM/DD HH:mm Z');//jshint ignore: line
+        piVm.userEdit.fecha_nacimiento = fecha_nac.toDate(); //jshint ignore: line
+      }
       ModalService.showModal({
         parentScope: $scope,
         fromTemplateUrl: 'templates/profile/info/edit.html'
