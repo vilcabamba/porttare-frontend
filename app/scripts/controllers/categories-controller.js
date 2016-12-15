9a285839a74a0ca238c4f0d718a34b1d9b02524e
Mario Andrés Correa <a@macool.me>
2016-12-14 19:07:42 -0500

    enhance categories displaying after hiding some of them in the api

--- app/scripts/controllers/categories-controller.js
+++ app/scripts/controllers/categories-controller.js
@@ -6,10 +6,11 @@
     .controller('CategoriesController', CategoriesController);
 
   function CategoriesController($scope) {
-
     var categoryVm = this;
     var cacheInit = moment();
-    categoryVm.categories = function () { return $scope.$parent.menuVm.categories; };
+
+    categoryVm.categories = parentScopeCategories;
+    categoryVm.categoryGridClassFor = categoryGridClassFor;
 
     $scope.$on('$ionicView.enter', function() {
       if (moment().diff(cacheInit, 'minutes') > 10) {
@@ -20,5 +21,23 @@
     $scope.$watch('$parent.menuVm.categories', function(){
       categoryVm.categories();
     });
+
+    function parentScopeCategories() {
+      return $scope.$parent.menuVm.categories;
+    }
+
+    function unevenCategories() {
+      return parentScopeCategories().length % 2 !== 0;
+    }
+
+    function categoryGridClassFor(index, last) {
+      var divisibleByThree = (index + 1) % 3 === 0,
+          fullWidth = divisibleByThree || (last && unevenCategories());
+      if (fullWidth) {
+        return 'grid-100';
+      } else {
+        return 'grid-50';
+      }
+    }
   }
 })();
