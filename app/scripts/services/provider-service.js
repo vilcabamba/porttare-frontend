101a01b586c5e42f1793de5c5dae0fcaa05d1bc1
Mario Mario <marioramos083@gmail.com>
2016-12-19 16:29:59 -0500

    Proveedor registro logotipo (#153)
    * GemFile.lock
    
    * proveedor con logotipo con ng-src & Upload.upload
    
    * sin offices, indentacion corregida
    
    * sin miggw, sin comentarios
    
    * apply directiva forma de pago, indentacion adaptacion a master
    
    * sin respuesta del Upload :/
    
    * crear perfil con logotipo - bug 808 & 809
    
    * offices and translate TODO

--- app/scripts/services/provider-service.js
+++ app/scripts/services/provider-service.js
@@ -5,7 +5,7 @@
     .module('porttare.services')
     .factory('ProviderService', ProviderService);
 
-  function ProviderService($http, $q, ENV) {
+  function ProviderService($http, $q, ENV, Upload) {
 
     var service = {
       createNewProvider: createNewProvider
@@ -14,11 +14,22 @@
     return service;
 
     function createNewProvider(data) {
-      return $http({
-        method: 'POST',
-        url: ENV.apiHost + '/api/provider/profile',
-        data: data
-      }).then(function success(res){
+      var promise;
+      var options = {
+                      method: 'POST',
+                      url: ENV.apiHost + '/api/provider/profile',
+                      data: data,
+                      arrayKey: '[]'
+                    };
+
+      if(data.logotipo){
+        promise = Upload.upload(options);
+      }
+      else{
+        promise = $http(options);
+      }
+      return promise
+      .then(function success(res){
         return res.data;
       }, function error(res) {
         return $q.reject(res.data);
