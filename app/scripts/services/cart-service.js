6ec9513b63bc08fcf7f81d1a490db669bba3957b
Amalia Anto <anahi2654@gmail.com>
2017-01-03 23:23:05 -0500

    cantidad maxima en carrito

--- app/scripts/services/cart-service.js
+++ app/scripts/services/cart-service.js
@@ -12,7 +12,9 @@
       checkout : checkout,
       updateOrderItem: updateOrderItem,
       removeOrderItem:removeOrderItem,
-      isCartEmpty: isCartEmpty
+      isCartEmpty: isCartEmpty,
+      findCartItem: findCartItem,
+      canAddItem: canAddItem
     };
 
     return service;
@@ -75,6 +77,32 @@
 
     function isCartEmpty(customerOrder){
       return !customerOrder || !customerOrder.provider_profiles || customerOrder.provider_profiles.length == 0; //jshint ignore:line
+    }
+
+    function findCartItem(customerOrder, itemId){
+      var orderItem = null;
+
+      if( !isCartEmpty(customerOrder) ){
+        for(var i = 0; i < customerOrder.provider_profiles.length && !orderItem; i++){ //jshint ignore:line
+          var providerProfile = customerOrder.provider_profiles[i]; //jshint ignore:line
+
+          for(var j=0; providerProfile.customer_order_items &&  i< providerProfile.customer_order_items.length && !orderItem; j++){ //jshint ignore:line
+            var customerOrderItem = providerProfile.customer_order_items[j]; //jshint ignore:line
+
+            if(customerOrderItem.id === itemId ){
+              orderItem = customerOrderItem;
+            }
+          }
+        }
+      }
+
+      return orderItem;
+    }
+
+    function canAddItem(orderItem, addCount, item){
+      var currentCount = orderItem ? orderItem.cantidad : 0;
+      var canAdd = ( currentCount + addCount <= item.cantidad);
+      return canAdd;
     }
   }
 })();
