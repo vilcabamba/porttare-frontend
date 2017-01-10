62ff39c0111fd9b4d28d93aed6a94f802391501a
carito22 <caritovaldiviezo22@gmail.com>
2017-01-09 23:28:38 -0500

    falta acomodar el icono y etiqueta abierto

--- app/scripts/directives/provider-profile-schedule/provider-profile-schedule.js
+++ app/scripts/directives/provider-profile-schedule/provider-profile-schedule.js
@@ -19,18 +19,66 @@
 
     return directive;
 
-    function providerProfileScheduleController($translate) {
+    function providerProfileScheduleController($translate,APP) {
       // jshint validthis:true
-      var ppSVm = this,
-          providerOffices = ppSVm.providerProfile.provider_offices; // jshint ignore:line
+      var ppSVm = this;
+      ppSVm.getDayText = getDayText;
+      var providerOffices = ppSVm.providerProfile.provider_offices; // jshint ignore:line
+
+      function getDayText(dayOfWeek){
+        var dayText = '';
+        switch (dayOfWeek){
+          case 0:
+          case 7:
+            dayText = "sun";
+            break;
+          case 1:
+            dayText = "mon";
+            break;
+          case 2:
+            dayText = "tue";
+            break;
+          case 3:
+            dayText = "wed";
+            break;
+          case 4:
+            dayText = "thu";
+            break;
+          case 5:
+            dayText = "fri";
+            break;
+          case 6:
+            dayText = "sat";
+            break;
+        };
+        return dayText;
+      };
 
       if (providerOffices.length > 0) {
         var mainOffice = providerOffices[0];
+        var fechaActual=new Date();
+        var dia=fechaActual.getDay();
+        var textDay=getDayText(dia);
 
-        // jshint ignore:start
-        ppSVm.openingTime = mainOffice.hora_de_apertura;
-        ppSVm.closingTime = mainOffice.hora_de_cierre;
-        // jshint ignore:end
+        var office=mainOffice.weekdays.filter(function (wday){
+          if(wday.day===textDay){
+            return wday;
+          }
+        });
+
+        if(office.length!==0){
+          // jshint ignore:start
+          ppSVm.openingTime = office[0].hora_de_apertura;
+          ppSVm.closingTime = office[0].hora_de_cierre;
+          // jshint ignore:end
+          if(office[0].abierto){
+            ppSVm.openStatus='Abierto';
+          }else{
+            ppSVm.openStatus='Cerrado';
+          }
+        }else{
+          ppSVm.openStatus='Cerrado';
+        }
 
         $translate(
           'globals.shortDayNames.' + mainOffice.inicio_de_labores // jshint ignore:line
