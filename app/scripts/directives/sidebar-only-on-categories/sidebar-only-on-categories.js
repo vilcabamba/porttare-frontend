5875ca2572bd12b7a83c66ce63893fe564f69ced
Mario Andrés Correa <a@macool.me>
2017-01-15 17:10:38 -0500

    tweaking sidebar

---
+++ app/scripts/directives/sidebar-only-on-categories/sidebar-only-on-categories.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('porttare.directives')
+    .directive('sidebarOnlyOnCategories', sidebarOnlyOnCategories);
+
+  function sidebarOnlyOnCategories(){
+    var directive = {
+      restrict: 'E',
+      transclude: true,
+      controllerAs: 'sidebarOnlyVM',
+      bindToController: true,
+      controller: ['$location', '$rootScope', sidebarOnlyOnCategoriesController],
+      templateUrl: 'templates/directives/sidebar-only-on-categories/sidebar-only-on-categories.html'
+    };
+    return directive;
+  }
+
+  function sidebarOnlyOnCategoriesController($location,
+                                             $rootScope){
+    var sidebarOnlyVM = this;
+
+    $rootScope.$on('$stateChangeSuccess', initialize);
+
+    initialize();
+
+    function initialize(){
+      sidebarOnlyVM.shouldDisplayContent = getShouldDisplayContent();
+    }
+
+    function getShouldDisplayContent(){
+      var categoriesPath = /app\/categories/;
+      return $location.path().match(categoriesPath);
+    }
+  }
+})();
