4d90b91ce0acd6c4db2d15dbf2150670610a2c7e
Fabricio Flores <fabrifloresg@gmail.com>
2017-01-21 16:11:16 +0100

    Split routes file

---
+++ app/scripts/config/routes/courier-routes.js
@@ -0,0 +1,52 @@
+'use strict';
+
+angular
+  .module('porttare.config')
+  .config(courierRoutes);
+
+function courierRoutes($stateProvider) {
+  $stateProvider
+  .state('courier', {
+    url: '/courier',
+    abstract: true,
+    templateUrl: 'templates/menu/menu-courier.html'
+  })
+  .state('courier.orders', {
+    url: '/orders',
+    views: {
+      'menuContent@courier': {
+        templateUrl: 'templates/courier/orders.html',
+        controller: 'OrdersController',
+        controllerAs: 'orVm',
+        resolve: {
+          shippingRequests: function (ShippingRequestService) {
+            return ShippingRequestService.getShippingRequestsWithStatus('new');
+          }
+        }
+      }
+    }
+  })
+  .state('courier.order', {
+    url: '/orders/:id',
+    cache: false,
+    params: {
+      order: null
+    },
+    views: {
+      'menuContent@courier': {
+        templateUrl: 'templates/courier/orders/show.html',
+        controller: 'CourierOrderController',
+        controllerAs: 'coVm',
+        resolve: {
+          courierOrder: function ($stateParams, ShippingRequestService) {
+            if ($stateParams.order) {
+              return $stateParams.order;
+            } else {
+              return ShippingRequestService.getShippingRequest($stateParams.id);
+            }
+          }
+        }
+      }
+    }
+  });
+}
