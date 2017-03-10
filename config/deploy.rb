3829033e801e98a88d8af7d80caf2c68fc2589a3
Mario Andrés Correa <a@macool.me>
2017-03-09 21:54:40 -0500

    tweak deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ append :linked_files, '.env'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'www/.well-known'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'app/.well-known'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
