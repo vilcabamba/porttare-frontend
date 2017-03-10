9668fd268832590590f1a8db527d791876ef1f96
Mario Andrés Correa <a@macool.me>
2017-03-09 20:42:12 -0500

    migration support

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,13 @@ lock "3.7.1"
 
 set :keep_releases, 3
 set :application, "porttare-frontend"
-set :repo_url, 'git@github.com:noggalito/porttare-frontend.git'
+set :repo_url, 'git@github.com:vilcabamba/porttare-frontend.git'
 
 set :npm_flags, '--silent --no-progress'
 
 set :slackistrano, {
   channel: '#porttare',
-  webhook: 'https://hooks.slack.com/services/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'
+  webhook: 'https://hooks.slack.com/services/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'
 }
 
 set :rbenv_ruby, '2.3.0'
